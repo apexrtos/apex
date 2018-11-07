5b5886ba56d23cdc15f2fd42f748dc983f9bba0f
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2018-11-07 11:58:25 +1100

    sys/dev/arm/armv7m-systick: replace volatile with read() and write() calls

--- sys/dev/arm/armv7m-systick/armv7m-systick.c
+++ sys/dev/arm/armv7m-systick/armv7m-systick.c
@@ -11,26 +11,28 @@
 struct syst {
 	union syst_csr {
 		struct {
-			unsigned ENABLE : 1;
-			unsigned TICKINT : 1;
-			unsigned CLKSOURCE : 1;
-			unsigned : 13;
-			unsigned COUNTFLAG : 1;
+			uint32_t ENABLE : 1;
+			uint32_t TICKINT : 1;
+			uint32_t CLKSOURCE : 1;
+			uint32_t : 13;
+			uint32_t COUNTFLAG : 1;
 		};
+		uint32_t r;
 	} CSR;
 	uint32_t RVR;
 	uint32_t CVR;
 	union syst_calib {
 		struct {
-			unsigned TENMS : 24;
-			unsigned : 6;
-			unsigned SKEW : 1;
-			unsigned NOREF : 1;
+			uint32_t TENMS : 24;
+			uint32_t : 6;
+			uint32_t SKEW : 1;
+			uint32_t NOREF : 1;
 		};
+		uint32_t r;
 	} CALIB;
 };
 static_assert(sizeof(struct syst) == 16, "Bad SYST size");
-static volatile struct syst *const SYST = (struct syst*)0xe000e010;
+static struct syst *const SYST = (struct syst*)0xe000e010;
 
 /*
  * Initialise
@@ -42,15 +44,15 @@ arm_armv7m_systick_init(const struct arm_armv7m_systick_desc *d)
 	assert(!SYST->CSR.ENABLE);
 
 	/* set systick timer to interrupt us at CONFIG_HZ */
-	SYST->RVR = d->clock / CONFIG_HZ - 1;
-	SYST->CVR = 0;
+	write32(&SYST->RVR, d->clock / CONFIG_HZ - 1);
+	write32(&SYST->CVR, 0);
 
 	/* enable timer & interrupts */
-	SYST->CSR = (union syst_csr){
+	write32(&SYST->CSR, (union syst_csr){
 		.ENABLE = 1,
 		.TICKINT = 1,
 		.CLKSOURCE = d->clksource,
-	};
+	}.r);
 
 	dbg("ARMv7-M SysTick initialised, RVR=%u\n", SYST->RVR);
 }
@@ -70,9 +72,9 @@ clock_ns_since_tick(void)
 	uint32_t cvr;
 	bool tick_pending;
 	do {
-		tick_pending = SCB->ICSR.PENDSTSET;
-		cvr = SYST->CVR;
-	} while (tick_pending != SCB->ICSR.PENDSTSET);
+		tick_pending = read32(&SCB->ICSR).PENDSTSET;
+		cvr = read32(&SYST->CVR);
+	} while (tick_pending != read32(&SCB->ICSR).PENDSTSET);
 
 	/* convert count to nanoseconds */
 	/* REVISIT: fractional multiply instead of 64-bit division? */
