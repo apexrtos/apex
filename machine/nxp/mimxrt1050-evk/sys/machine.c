d065d941807dd05913cca847d12f9facbcd0e599
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2018-12-11 15:28:31 +1100

    cpu/nxp/imxrt10xx: import GPIO definitions

--- machine/nxp/mimxrt1050-evk/sys/machine.c
+++ machine/nxp/mimxrt1050-evk/sys/machine.c
@@ -4,6 +4,7 @@
 #include <conf/drivers.h>
 #include <cpu.h>
 #include <cpu/nxp/imxrt10xx/ccm.h>
+#include <cpu/nxp/imxrt10xx/gpio.h>
 #include <cpu/nxp/imxrt10xx/iomuxc.h>
 #include <cpu/nxp/imxrt10xx/pmu.h>
 #include <cpu/nxp/imxrt10xx/src.h>
