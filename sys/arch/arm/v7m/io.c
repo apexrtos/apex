02f20b500ddda9b707529be87cfda30470806973
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2019-01-14 12:23:18 +1100

    sys/arch/arm/v7m: follow renames in sys/include/arch.h

--- sys/arch/arm/v7m/io.c
+++ sys/arch/arm/v7m/io.c
@@ -31,7 +31,7 @@ write_memory_barrier(void)
  * Read uint8_t from memory location p
  */
 inline uint8_t
-io_read8(const uint8_t *p)
+mmio_read8(const uint8_t *p)
 {
 	uint8_t v;
 	asm volatile("ldrb %0, %1" : "=r"(v) : "m"(*p));
@@ -42,7 +42,7 @@ io_read8(const uint8_t *p)
  * Read uint16_t from memory location p
  */
 inline uint16_t
-io_read16(const uint16_t *p)
+mmio_read16(const uint16_t *p)
 {
 	uint16_t v;
 	asm volatile("ldrh %0, %1" : "=r"(v) : "m"(*p));
@@ -53,7 +53,7 @@ io_read16(const uint16_t *p)
  * Read uint32_t from memory location p
  */
 inline uint32_t
-io_read32(const uint32_t *p)
+mmio_read32(const uint32_t *p)
 {
 	uint32_t v;
 	asm volatile("ldr %0, %1" : "=r"(v) : "m"(*p));
@@ -64,7 +64,7 @@ io_read32(const uint32_t *p)
  * Write uint8_t to memory location p
  */
 inline void
-io_write8(uint8_t *p, uint8_t v)
+mmio_write8(uint8_t *p, uint8_t v)
 {
 	asm volatile("strb %1, %0" : "=m"(*p) : "r"(v));
 }
@@ -73,7 +73,7 @@ io_write8(uint8_t *p, uint8_t v)
  * Write uint16_t to memory location p
  */
 inline void
-io_write16(uint16_t *p, uint16_t v)
+mmio_write16(uint16_t *p, uint16_t v)
 {
 	asm volatile("strh %1, %0" : "=m"(*p) : "r"(v));
 }
@@ -82,7 +82,7 @@ io_write16(uint16_t *p, uint16_t v)
  * Write uint32_t to memory location p
  */
 inline void
-io_write32(uint32_t *p, uint32_t v)
+mmio_write32(uint32_t *p, uint32_t v)
 {
 	asm volatile("str %1, %0" : "=m"(*p) : "r"(v));
 }
