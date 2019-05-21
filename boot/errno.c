3e5bd783b8d1a6922d4226db6f9a75e3a8f0d090
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2019-05-21 12:43:34 +1000

    boot/errno: update for musl v1.1.22

--- boot/errno.c
+++ boot/errno.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <sys/include/compiler.h>
 
 int *
 __errno_location(void)
@@ -6,3 +7,5 @@ __errno_location(void)
 	static int v;
 	return &v;
 }
+
+weak_alias(__errno_location, ___errno_location);
