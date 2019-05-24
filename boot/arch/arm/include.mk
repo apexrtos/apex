8eb63a7a12864eea88c82c89e496062feee2b21f
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2019-05-24 16:25:00 +1000

    boot/arch/arm: avoid generating duplicate make rules
    The cache.c and io.c source files are compiled by both the bootloader
    and kernel. However, it is not guaranteed that they will be compiled
    with the same set of compilation options.
    
    By symlinking these files the intent is clear: we want to compile them
    twice.

--- boot/arch/arm/include.mk
+++ boot/arch/arm/include.mk
@@ -1,8 +1,8 @@
 SOURCES += \
+    $(ARCHDIR)/$(CONFIG_SUBARCH)_cache.c \
     $(ARCHDIR)/$(CONFIG_SUBARCH)_head.S \
+    $(ARCHDIR)/$(CONFIG_SUBARCH)_io.c \
     $(ARCHDIR)/crt_init.S \
-    $(CONFIG_APEXDIR)/sys/$(ARCHDIR)/$(CONFIG_SUBARCH)/cache.c \
-    $(CONFIG_APEXDIR)/sys/$(ARCHDIR)/$(CONFIG_SUBARCH)/io.c \
 
 INCLUDE += \
     $(CONFIG_APEXDIR)/sys/$(ARCHDIR)/include/$(CONFIG_SUBARCH) \
