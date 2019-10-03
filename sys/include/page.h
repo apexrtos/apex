apexrtos/apex

commit 85f70d1f1481d10c166b29788b4e135f04096f29

sys/mem/vm: fix occasional spurious ENOMEM
Previously, mmap could spuriously fail when attempting to change an
existing mapping. This was possible because the pages underlying the
mapping were released then reacquired, allowing another thread to
allocate them.

Now that the page allocator supports extending a reservation we can
avoid the release/acquire.

sys/mem/vm.cpp
@@ -18,6 +18,7 @@
 
 /*
  * TODO: shared mappings
+ *	 mmap can leave address space inconsistent on OOM
  */
 
 struct seg {
@@ -139,7 +140,7 @@ seg_insert(seg *prev, std::unique_ptr<phys> pages, size_t len, int prot,
  * Must be called with address space write lock held.
  */
 static int
-do_munmapfor(as *a, void *const vaddr, const size_t ulen)
+do_munmapfor(as *a, void *const vaddr, const size_t ulen, bool remap)
 {
 	int err = 0;
 
@@ -160,7 +161,9 @@ do_munmapfor(as *a, void *const vaddr, const size_t ulen)
 			break;
 		if (s->base >= uaddr && send <= uend) {
 			/* entire segment */
-			err = as_unmap(a, s->base, s->len, s->vn, s->off);
+			if (!remap)
+				err = as_unmap(a, s->base, s->len, s->vn,
+				    s->off);
 			list_remove(&s->link);
 			if (s->vn)
 				vn_close(s->vn);
@@ -171,8 +174,9 @@ do_munmapfor(as *a, void *const vaddr, const size_t ulen)
 			if (!(ns = (seg*)kmem_alloc(sizeof(seg), MA_FAST)))
 				return DERR(-ENOMEM);
 			s->len = uaddr - (char*)s->base;
-			err = as_unmap(a, uaddr, ulen, s->vn, s->off + s->len);
-
+			if (!remap)
+				err = as_unmap(a, uaddr, ulen, s->vn,
+				    s->off + s->len);
 			*ns = *s;
 			ns->base = uend;
 			ns->len = send - uend;
@@ -186,12 +190,15 @@ do_munmapfor(as *a, void *const vaddr, const size_t ulen)
 		} else if (s->base < uaddr) {
 			/* end of segment */
 			const auto l = uaddr - (char*)s->base;
-			err = as_unmap(a, uaddr, s->len - l, s->vn, s->off + l);
+			if (!remap)
+				err = as_unmap(a, uaddr, s->len - l, s->vn,
+				    s->off + l);
 			s->len = l;
 		} else if (s->base < uend) {
 			/* start of segment */
 			const auto l = uend - (char*)s->base;
-			err = as_unmap(a, s->base, l, s->vn, s->off);
+			if (!remap)
+				err = as_unmap(a, s->base, l, s->vn, s->off);
 			if (s->vn)
 				s->off += l;
 			s->base = (char*)s->base + l;
@@ -215,9 +222,7 @@ static void *
 do_mmapfor(as *a, void *addr, size_t len, int prot, int flags, int fd,
     off_t off, long attr)
 {
-	int err;
 	std::unique_ptr<vnode> vn;
-	const bool fixed = flags & MAP_FIXED;
 	const bool anon = flags & MAP_ANONYMOUS;
 	const bool priv = flags & MAP_PRIVATE;
 	const bool shared = flags & MAP_SHARED;
@@ -234,9 +239,6 @@ do_mmapfor(as *a, void *addr, size_t len, int prot, int flags, int fd,
 		if (!vn.get())
 			return (void*)DERR(-EBADF);
 	}
-
-	if (fixed && (err = do_munmapfor(a, addr, len)) < 0)
-		return (void*)err;
 
 	return as_map(a, addr, len, prot, flags, std::move(vn), off, attr);
 }
@@ -266,7 +268,7 @@ munmapfor(as *a, void *const vaddr, const size_t ulen)
 	if (int err = l.lock(); err < 0)
 		return err;
 
-	return do_munmapfor(a, vaddr, ulen);
+	return do_munmapfor(a, vaddr, ulen, false);
 }
 
 /*
@@ -459,7 +461,7 @@ sc_brk(void *addr)
 	/* shrink */
 	if (addr < a->brk)
 		if (auto r = do_munmapfor(a, addr,
-		    (uintptr_t)a->brk - (uintptr_t)addr); r < 0)
+		    (uintptr_t)a->brk - (uintptr_t)addr, false); r < 0)
 			return (void*)r;
 
 	/* grow */
@@ -682,6 +684,14 @@ int
 as_insert(as *a, std::unique_ptr<phys> pages, size_t len, int prot,
     int flags, std::unique_ptr<vnode> vn, off_t off, long attr)
 {
+	int err;
+	const bool fixed = flags & MAP_FIXED;
+
+	/* remove any existing mappings */
+	if (fixed && (err = do_munmapfor(a, pages.get(), len, true)) < 0)
+		return err;
+
+	/* find insertion point */
 	seg *s;
 	list_for_each_entry(s, &a->segs, link) {
 		if (s->base > pages.get())
@@ -689,12 +699,14 @@ as_insert(as *a, std::unique_ptr<phys> pages, size_t len, int prot,
 	}
 	s = list_entry(list_prev(&s->link), seg, link);
 
+	/* attempt to extend existing segment */
 	if (!list_end(&a->segs, &s->link) &&
 	    seg_extend(s, pages.get(), len, prot, vn.get(), off, attr)) {
 		pages.release();
 		return 0;
 	}
 
+	/* insert new segment */
 	return seg_insert(s, std::move(pages), len, prot,
 	    std::move(vn), off, attr);
 }

commit e760a3f549c841934bef1ab0e9f4fd52dc6c1aa8

apex: prevent sc_brk from reusing mmaped memory
The combination of c29fca178019c465f6eeb5a58c78ce7da07dcadd and
85f70d1f1481d10c166b29788b4e135f04096f29 introduced a bug where sc_brk
could reuse memory which had been allocated with mmap.

sys/include/page.h
@@ -8,9 +8,10 @@ struct bootargs;
 /*
  * Page allocation flags
  */
+#define PAF_REALLOC 0x20000000		    /* extend existing allocation */
 #define PAF_MAPPED 0x40000000		    /* page is part of a vm mapping */
 #define PAF_NO_SPEED_FALLBACK 0x80000000    /* do not use alternate speed */
-#define PAF_MASK 0xc0000000
+#define PAF_MASK 0xe0000000
 
 struct meminfo {
 	phys *base;		    /* start address */
@@ -24,7 +25,7 @@ extern "C" {
 
 phys   *page_alloc_order(size_t order, long ma_paf, void *);
 phys   *page_alloc(size_t, long ma_paf, void *);
-phys   *page_reserve(phys *, size_t, void *);
+phys   *page_reserve(phys *, size_t, long paf, void *);
 int	page_free(phys *, size_t, void *);
 bool	page_valid(phys *, size_t, void *);
 long	page_attr(phys *, size_t len);

sys/mem/page.cpp
@@ -376,7 +376,7 @@ page_alloc(size_t len, long attr, void *owner)
  */
 static phys *
 page_reserve(region &r, phys *const addr, const size_t len,
-    const PG_STATE st, void *owner)
+    const PG_STATE st, long attr, void *owner)
 {
 	assert(st == PG_HOLE || st == PG_SYSTEM || st == PG_FIXED);
 	if (len == 0)
@@ -388,9 +388,10 @@ page_reserve(region &r, phys *const addr, const size_t len,
 
 	/* check that range is free or overlaps existing allocation */
 	for (auto i = begin; i != end; ++i) {
-		if (r.pages[i].state == st && r.pages[i].owner == owner)
-			continue;
 		if (r.pages[i].state == PG_FREE)
+			continue;
+		if (r.pages[i].state == st && r.pages[i].owner == owner &&
+		    attr & PAF_REALLOC)
 			continue;
 		return 0;
 	}
@@ -413,12 +414,12 @@ page_reserve(region &r, phys *const addr, const size_t len,
  * returns 0 on failure, physical address otherwise.
  */
 static phys *
-page_reserve(phys *addr, size_t len, const PG_STATE st, void *owner)
+page_reserve(phys *addr, size_t len, const PG_STATE st, long attr, void *owner)
 {
 	auto *r = find_region(addr, len);
 	if (!r)
 		return 0;
-	return page_reserve(*r, addr, len, st, owner);
+	return page_reserve(*r, addr, len, st, attr, owner);
 }
 
 /*
@@ -429,13 +430,15 @@ page_reserve(phys *addr, size_t len, const PG_STATE st, void *owner)
  * returns 0 on failure, physical address otherwise.
  */
 phys *
-page_reserve(phys *addr, size_t len, void *owner)
+page_reserve(phys *addr, size_t len, long attr, void *owner)
 {
+	const auto st = attr & PAF_MAPPED ? PG_MAPPED : PG_FIXED;
+
 	auto *r = find_region(addr, len);
 	if (!r)
 		return 0;
 	std::lock_guard l(r->lock);
-	return page_reserve(*r, addr, len, PG_FIXED, owner);
+	return page_reserve(*r, addr, len, st, attr, owner);
 }
 
 /*
@@ -695,9 +698,9 @@ page_init(const meminfo *mi, const size_t mi_size, const bootargs *args)
 		list_insert(&r.blocks[r.nr_orders - 1], &r.pages[0].link);
 
 		/* reserve pages without physical backing */
-		if (!page_reserve(r, r.base, r.begin - r.base, PG_HOLE, nullptr))
+		if (!page_reserve(r, r.base, r.begin - r.base, PG_HOLE, 0, nullptr))
 			panic("bad meminfo");
-		if (!page_reserve(r, r.end, r.base + r.size - r.end, PG_HOLE, nullptr))
+		if (!page_reserve(r, r.end, r.base + r.size - r.end, PG_HOLE, 0, nullptr))
 			panic("bad meminfo");
 
 		dbg("page_init: region %zu: %p -> %p covering %p -> %p\n",
@@ -714,7 +717,7 @@ page_init(const meminfo *mi, const size_t mi_size, const bootargs *args)
 
 	/* reserve unusable memory */
 	for_each_reserved_region([&](phys *p, size_t len) {
-		auto r = page_reserve(p, len, PG_SYSTEM, &kern_task);
+		auto r = page_reserve(p, len, PG_SYSTEM, 0, &kern_task);
 		/* reservation can fail for ROM addresses or other unmappable
 		 * memory */
 		dbg("page_init: reserve %p -> %p %s\n", p, p + len,
@@ -722,7 +725,7 @@ page_init(const meminfo *mi, const size_t mi_size, const bootargs *args)
 	});
 
 	/* reserve memory allocated for region & page structures */
-	if (!page_reserve(m_begin, m_alloc - m_begin, PG_SYSTEM, &page_id))
+	if (!page_reserve(m_begin, m_alloc - m_begin, PG_SYSTEM, 0, &page_id))
 		panic("bug");
 
 	/* initialise regions_by_speed */

sys/mem/untranslated.cpp
@@ -88,7 +88,7 @@ as_map(struct as *as, void *addr, size_t len, int prot, int flags,
 	const auto fixed = flags & MAP_FIXED;
 
 	std::unique_ptr<phys> pages(fixed
-	    ? page_reserve((phys*)addr, len, as)
+	    ? page_reserve((phys*)addr, len, attr, as)
 	    : page_alloc(len, attr, as),
 	    {len, as});
 

sys/mem/vm.cpp
@@ -255,6 +255,9 @@ mmapfor(as *a, void *addr, size_t len, int prot, int flags, int fd, off_t off,
 	if (int err = l.lock(); err < 0)
 		return (void*)err;
 
+	/* mmap replaces existing mappings */
+	attr |= PAF_REALLOC;
+
 	return do_mmapfor(a, addr, len, prot, flags, fd, off, attr);
 }
 