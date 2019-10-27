6f8fce5ed60c99475c8286e9aa57a0c81ab7d5e7
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2019-10-28 10:46:23 +1100

    sys/fs/util: c++ version of for_each_iov

--- sys/fs/util.h
+++ sys/fs/util.h
@@ -1,11 +1,11 @@
 #ifndef fs_util_h
 #define fs_util_h
 
+#include <sys/uio.h>
 #include <types.h>
 
 struct dirent;
 struct file;
-struct iovec;
 
 #if defined(__cplusplus)
 extern "C" {
@@ -25,6 +25,26 @@ ssize_t	for_each_iov(struct file *, const struct iovec *, size_t, off_t,
 		     ssize_t (*fn)(struct file *, void *, size_t, off_t));
 
 #if defined(__cplusplus)
+}
+
+/*
+ * for_each_iov - C++ version of for_each_iov
+ */
+template<typename Fn>
+ssize_t
+for_each_iov(file *fp, const iovec *iov, size_t count, off_t offset, Fn &&fn)
+{
+	ssize_t total = 0;
+	ssize_t res = 0;
+	while (count--) {
+		if ((res = fn(iov->iov_base, iov->iov_len, offset + total)) < 0)
+			break;
+		total += res;
+		if ((size_t)res != iov->iov_len)
+			return total;
+		++iov;
+	}
+	return total > 0 ? total : res;
 }
 #endif
 
