ac44f98b4a7682a18df0689efcc5e289c2eb0921
Richard Tucker <rtucker@mookins.com>
2019-10-31 11:11:45 +1100

    machine/nxp/imxrt1050-evkb: move tools to cpu/nxp/imxrt10xx
    Tools are common across all boards using imxrt CPUs.

--- machine/nxp/imxrt1050-evkb/gdb.mk
+++ machine/nxp/imxrt1050-evkb/gdb.mk
@@ -5,5 +5,5 @@ gdb:
 	CPU=$(CONFIG_CPU) \
 	BOARD=$(CONFIG_BOARD) \
 	BUILDDIR=$(CONFIG_BUILDDIR) \
-	TOOLSDIR=$(CONFIG_APEXDIR)/machine/nxp/imxrt1050-evkb/tools \
+	TOOLSDIR=$(CONFIG_APEXDIR)/cpu/nxp/imxrt10xx/tools \
 	arm-none-eabi-gdb -x $(CONFIG_APEXDIR)/machine/nxp/imxrt1050-evkb/gdbinit
