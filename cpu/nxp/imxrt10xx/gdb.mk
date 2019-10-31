6bf0f517ef3925258072557dd23599b137eaff13
Richard Tucker <rtucker@mookins.com>
2019-10-31 11:33:34 +1100

    machine/nxp/imxrt1050-evkb: move gdb files to cpu/nxp/imxrt10xx
    GDB setup is common across all boards using imxrt CPUs.

--- cpu/nxp/imxrt10xx/gdb.mk
+++ cpu/nxp/imxrt10xx/gdb.mk
@@ -6,4 +6,4 @@ gdb:
 	BOARD=$(CONFIG_BOARD) \
 	BUILDDIR=$(CONFIG_BUILDDIR) \
 	TOOLSDIR=$(CONFIG_APEXDIR)/cpu/nxp/imxrt10xx/tools \
-	arm-none-eabi-gdb -x $(CONFIG_APEXDIR)/machine/nxp/imxrt1050-evkb/gdbinit
+	arm-none-eabi-gdb -x $(CONFIG_APEXDIR)/cpu/nxp/imxrt10xx/gdbinit
