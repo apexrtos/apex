7545eec64150a0c2e3ec88a56059f214b72c54df
Richard Tucker <rtucker@mookins.com>
2019-10-31 14:42:30 +1100

    machine/nxp/imxrt1050-evkb: move common boot files to cpu/nxp/imxrt10xx/boot

--- machine/nxp/imxrt1050-evkb/boot/include.mk
+++ machine/nxp/imxrt1050-evkb/boot/include.mk
@@ -1,4 +1,4 @@
 SOURCES += \
 	$(CONFIG_APEXDIR)/machine/nxp/imxrt1050-evkb/boot/machine.c \
-	$(CONFIG_APEXDIR)/machine/nxp/imxrt1050-evkb/boot/machine_early_memory_init.S \
+	$(CONFIG_APEXDIR)/cpu/nxp/imxrt10xx/boot/machine_early_memory_init.S \
 	$(CONFIG_APEXDIR)/machine/nxp/imxrt1050-evkb/boot/cache.c \
