ed90deb234efe2c3a964aedf6da805910f1f3d0a
Richard Tucker <rtucker@mookins.com>
2019-10-31 14:48:38 +1100

    machine/nxp/imxrt1050-evkb: move common sys files to cpu/nxp/imxrt10xx/sys

--- machine/nxp/imxrt1050-evkb/sys/include.mk
+++ machine/nxp/imxrt1050-evkb/sys/include.mk
@@ -1,5 +1,5 @@
 LDSCRIPT := $(MACHINEDIR)/kernel.ld
 
 SOURCES += \
-	$(CONFIG_APEXDIR)/machine/nxp/imxrt1050-evkb/sys/cache.c \
+	$(CONFIG_APEXDIR)/cpu/nxp/imxrt10xx/sys/cache.c \
 	$(CONFIG_APEXDIR)/machine/nxp/imxrt1050-evkb/sys/machine.c \
