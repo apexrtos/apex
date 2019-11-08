ac1bbd32fa1dde8360423c9c1892b5b87e7617b6
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2019-11-08 14:23:30 +1100

    imxrt1050-evkb: use CONFIG_MACHINEDIR

--- machine/nxp/imxrt1050-evkb/boot/include.mk
+++ machine/nxp/imxrt1050-evkb/boot/include.mk
@@ -1,4 +1,4 @@
 SOURCES += \
-	$(CONFIG_APEXDIR)/machine/nxp/imxrt1050-evkb/boot/machine.c \
+	$(CONFIG_MACHINEDIR)/boot/machine.c \
 	$(CONFIG_APEXDIR)/cpu/nxp/imxrt10xx/boot/machine_early_memory_init.S \
 	$(CONFIG_APEXDIR)/cpu/nxp/imxrt10xx/boot/cache.c \
--- machine/nxp/imxrt1050-evkb/sys/include.mk
+++ machine/nxp/imxrt1050-evkb/sys/include.mk
@@ -2,4 +2,4 @@ LDSCRIPT := $(CONFIG_MACHINEDIR)/sys/kernel.ld
 
 SOURCES += \
 	$(CONFIG_APEXDIR)/cpu/nxp/imxrt10xx/sys/cache.c \
-	$(CONFIG_APEXDIR)/machine/nxp/imxrt1050-evkb/sys/machine.c \
+	$(CONFIG_MACHINEDIR)/sys/machine.c \
