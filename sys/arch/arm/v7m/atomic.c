2a35f075ced43a40a2d06392f925800e0254bf0c
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2019-12-16 15:48:51 +1100

    arm/v7m: don't duplicate gcc builtin atomics

--- sys/arch/arm/v7m/atomic.c
+++ sys/arch/arm/v7m/atomic.c
@@ -1,10 +1,5 @@
 #include <stddef.h>
 
-/*
- * TODO: sometimes gcc doesn't use our implementations, see
- *       https://gcc.gnu.org/bugzilla/show_bug.cgi?id=88456
- */
-
 #include <irq.h>
 #include <string.h>
 
@@ -20,17 +15,6 @@
 /*
  * atomic_load
  */
-#define ATOMIC_LOAD(N, T) \
-inline T \
-__atomic_load_##N(const volatile T *p, int m) \
-{ \
-	return *p; \
-}
-
-ATOMIC_LOAD(1, uint8_t)
-ATOMIC_LOAD(2, uint16_t)
-ATOMIC_LOAD(4, uint32_t)
-
 inline uint64_t
 __atomic_load_8(const volatile uint64_t *p, int m)
 {
@@ -58,17 +42,6 @@ __atomic_load(size_t len, const void *p, void *r, int m)
 /*
  * atomic_store
  */
-#define ATOMIC_STORE(N, T) \
-inline void \
-__atomic_store_##N(volatile T *p, T v, int m) \
-{ \
-	*p = v; \
-}
-
-ATOMIC_STORE(1, uint8_t)
-ATOMIC_STORE(2, uint16_t)
-ATOMIC_STORE(4, uint32_t)
-
 inline void
 __atomic_store_8(volatile uint64_t *p, uint64_t v, int m)
 {
@@ -94,28 +67,6 @@ __atomic_store(size_t len, void *p, const void *v, int m)
 /*
  * atomic_exchange
  */
-#define ATOMIC_EXCHANGE(N, T, S) \
-inline T \
-__atomic_exchange_##N(volatile T *p, T v, int m) \
-{ \
-	int ex; \
-	T ret; \
-	asm volatile( \
-		"1: ldrex"S" %[ret], %[p]\n" \
-		"strex"S" %[ex], %[v], %[p]\n" \
-		"cmp %[ex], 0\n" \
-		"bne 1b\n" \
-		: [ex]"=&lh"(ex), [p]"=Q"(*p), [ret]"=&lh"(ret) \
-		: [v]"lh"(v), "m"(*p) \
-		: "cc" \
-	); \
-	return ret; \
-}
-
-ATOMIC_EXCHANGE(1, uint8_t, "b")
-ATOMIC_EXCHANGE(2, uint16_t, "h")
-ATOMIC_EXCHANGE(4, uint32_t, "")
-
 inline uint64_t
 __atomic_exchange_8(volatile uint64_t *p, uint64_t v, int m)
 {
@@ -145,34 +96,6 @@ __atomic_exchange(size_t len, void *p, const void *v, void *r, int m)
 /*
  * atomic_compare_exchange
  */
-#define ATOMIC_COMPARE_EXCHANGE(N, T, S) \
-inline bool \
-__atomic_compare_exchange_##N(volatile T *p, const T *e, T d, bool w, int sm, int fm) \
-{ \
-	int ex; \
-	T cur; \
-	bool ret; \
-	asm volatile( \
-		"1: ldrex"S" %[cur], %[p]\n" \
-		"cmp %[cur], %[e]\n" \
-		"bne 2f\n" \
-		"strex"S" %[ex], %[d], %[p]\n" \
-		"cmp %[ex], 0\n" \
-		"bne 1b\n" \
-		"2: ite eq\n" \
-		"moveq %[ret], 1\n" \
-		"movne %[ret], 0\n" \
-		: [ex]"=&lh"(ex), [p]"=Q"(*p), [cur]"=&lh"(cur), [ret]"=lh"(ret) \
-		: [e]"lhI"(*e), [d]"lh"(d), "m"(*p) \
-		: "cc" \
-	); \
-	return ret; \
-}
-
-ATOMIC_COMPARE_EXCHANGE(1, uint8_t, "b")
-ATOMIC_COMPARE_EXCHANGE(2, uint16_t, "h")
-ATOMIC_COMPARE_EXCHANGE(4, uint32_t, "")
-
 inline bool
 __atomic_compare_exchange_8(volatile uint64_t *p, const uint64_t *e, uint64_t d,
     bool weak, int sm, int fm)
@@ -198,34 +121,8 @@ __atomic_compare_exchange(size_t len, void *p, const void *e, void *d,
 }
 
 /*
- * ATOMIC_OP_FETCH
- */
-#define ATOMIC_OP_FETCH(OP, INS, N, T, S) \
-inline T \
-__atomic_##OP##_fetch_##N(volatile T *p, T v, int m) \
-{ \
-	int ex; \
-	T ret; \
-	asm volatile( \
-		"1: ldrex"S" %[ret], %[p]\n" \
-		INS \
-		"strex"S" %[ex], %[ret], %[p]\n" \
-		"cmp %[ex], 0\n" \
-		"bne 1b\n" \
-		: [ex]"=&lh"(ex), [p]"=Q"(*p), [ret]"=&lh"(ret) \
-		: [v]"lhI"(v), "m"(*p) \
-		: "cc" \
-	); \
-	return ret; \
-}
-
-/*
  * atomic_add_fetch
  */
-ATOMIC_OP_FETCH(add, "add %[ret], %[v]\n", 1, uint8_t, "b")
-ATOMIC_OP_FETCH(add, "add %[ret], %[v]\n", 2, uint16_t, "h")
-ATOMIC_OP_FETCH(add, "add %[ret], %[v]\n", 4, uint32_t, "")
-
 inline uint64_t
 __atomic_add_fetch_8(volatile uint64_t *p, uint64_t v, int m)
 {
@@ -239,10 +136,6 @@ __atomic_add_fetch_8(volatile uint64_t *p, uint64_t v, int m)
 /*
  * atomic_sub_fetch
  */
-ATOMIC_OP_FETCH(sub, "sub %[ret], %[v]\n", 1, uint8_t, "b")
-ATOMIC_OP_FETCH(sub, "sub %[ret], %[v]\n", 2, uint16_t, "h")
-ATOMIC_OP_FETCH(sub, "sub %[ret], %[v]\n", 4, uint32_t, "")
-
 inline uint64_t
 __atomic_sub_fetch_8(volatile uint64_t *p, uint64_t v, int m)
 {
@@ -256,10 +149,6 @@ __atomic_sub_fetch_8(volatile uint64_t *p, uint64_t v, int m)
 /*
  * atomic_and_fetch
  */
-ATOMIC_OP_FETCH(and, "and %[ret], %[v]\n", 1, uint8_t, "b")
-ATOMIC_OP_FETCH(and, "and %[ret], %[v]\n", 2, uint16_t, "h")
-ATOMIC_OP_FETCH(and, "and %[ret], %[v]\n", 4, uint32_t, "")
-
 inline uint64_t
 __atomic_and_fetch_8(volatile uint64_t *p, uint64_t v, int m)
 {
@@ -273,10 +162,6 @@ __atomic_and_fetch_8(volatile uint64_t *p, uint64_t v, int m)
 /*
  * atomic_xor_fetch
  */
-ATOMIC_OP_FETCH(xor, "eor %[ret], %[v]\n", 1, uint8_t, "b")
-ATOMIC_OP_FETCH(xor, "eor %[ret], %[v]\n", 2, uint16_t, "h")
-ATOMIC_OP_FETCH(xor, "eor %[ret], %[v]\n", 4, uint32_t, "")
-
 inline uint64_t
 __atomic_xor_fetch_8(volatile uint64_t *p, uint64_t v, int m)
 {
@@ -290,10 +175,6 @@ __atomic_xor_fetch_8(volatile uint64_t *p, uint64_t v, int m)
 /*
  * atomic_or_fetch
  */
-ATOMIC_OP_FETCH(or, "orr %[ret], %[v]\n", 1, uint8_t, "b")
-ATOMIC_OP_FETCH(or, "orr %[ret], %[v]\n", 2, uint16_t, "h")
-ATOMIC_OP_FETCH(or, "orr %[ret], %[v]\n", 4, uint32_t, "")
-
 inline uint64_t
 __atomic_or_fetch_8(volatile uint64_t *p, uint64_t v, int m)
 {
@@ -307,10 +188,6 @@ __atomic_or_fetch_8(volatile uint64_t *p, uint64_t v, int m)
 /*
  * atomic_nand_fetch
  */
-ATOMIC_OP_FETCH(nand, "and %[ret], %[v]\nmvn %[ret], %[ret]\n", 1, uint8_t, "b")
-ATOMIC_OP_FETCH(nand, "and %[ret], %[v]\nmvn %[ret], %[ret]\n", 2, uint16_t, "h")
-ATOMIC_OP_FETCH(nand, "and %[ret], %[v]\nmvn %[ret], %[ret]\n", 4, uint32_t, "")
-
 inline uint64_t
 __atomic_nand_fetch_8(volatile uint64_t *p, uint64_t v, int m)
 {
@@ -322,34 +199,8 @@ __atomic_nand_fetch_8(volatile uint64_t *p, uint64_t v, int m)
 }
 
 /*
- * ATOMIC_FETCH_OP
- */
-#define ATOMIC_FETCH_OP(OP, INS, N, T, S) \
-inline T \
-__atomic_fetch_##OP##_##N(volatile T *p, T v, int m) \
-{ \
-	int ex; \
-	T ret, tmp; \
-	asm volatile( \
-		"1: ldrex"S" %[ret], %[p]\n" \
-		INS \
-		"strex"S" %[ex], %[tmp], %[p]\n" \
-		"cmp %[ex], 0\n" \
-		"bne 1b\n" \
-		: [ex]"=&lh"(ex), [p]"=Q"(*p), [ret]"=&lh"(ret), [tmp]"=&lh"(tmp) \
-		: [v]"lhI"(v), "m"(*p) \
-		: "cc" \
-	); \
-	return ret; \
-}
-
-/*
  * atomic_fetch_add
  */
-ATOMIC_FETCH_OP(add, "add %[tmp], %[ret], %[v]\n", 1, uint8_t, "b")
-ATOMIC_FETCH_OP(add, "add %[tmp], %[ret], %[v]\n", 2, uint16_t, "h")
-ATOMIC_FETCH_OP(add, "add %[tmp], %[ret], %[v]\n", 4, uint32_t, "")
-
 inline uint64_t
 __atomic_fetch_add_8(volatile uint64_t *p, uint64_t v, int m)
 {
@@ -364,10 +215,6 @@ __atomic_fetch_add_8(volatile uint64_t *p, uint64_t v, int m)
 /*
  * atomic_fetch_sub
  */
-ATOMIC_FETCH_OP(sub, "sub %[tmp], %[ret], %[v]\n", 1, uint8_t, "b")
-ATOMIC_FETCH_OP(sub, "sub %[tmp], %[ret], %[v]\n", 2, uint16_t, "h")
-ATOMIC_FETCH_OP(sub, "sub %[tmp], %[ret], %[v]\n", 4, uint32_t, "")
-
 inline uint64_t
 __atomic_fetch_sub_8(volatile uint64_t *p, uint64_t v, int m)
 {
@@ -382,10 +229,6 @@ __atomic_fetch_sub_8(volatile uint64_t *p, uint64_t v, int m)
 /*
  * atomic_fetch_and
  */
-ATOMIC_FETCH_OP(and, "and %[tmp], %[ret], %[v]\n", 1, uint8_t, "b")
-ATOMIC_FETCH_OP(and, "and %[tmp], %[ret], %[v]\n", 2, uint16_t, "h")
-ATOMIC_FETCH_OP(and, "and %[tmp], %[ret], %[v]\n", 4, uint32_t, "")
-
 inline uint64_t
 __atomic_fetch_and_8(volatile uint64_t *p, uint64_t v, int m)
 {
@@ -400,10 +243,6 @@ __atomic_fetch_and_8(volatile uint64_t *p, uint64_t v, int m)
 /*
  * atomic_fetch_xor
  */
-ATOMIC_FETCH_OP(xor, "eor %[tmp], %[ret], %[v]\n", 1, uint8_t, "b")
-ATOMIC_FETCH_OP(xor, "eor %[tmp], %[ret], %[v]\n", 2, uint16_t, "h")
-ATOMIC_FETCH_OP(xor, "eor %[tmp], %[ret], %[v]\n", 4, uint32_t, "")
-
 inline uint64_t
 __atomic_fetch_xor_8(volatile uint64_t *p, uint64_t v, int m)
 {
@@ -418,10 +257,6 @@ __atomic_fetch_xor_8(volatile uint64_t *p, uint64_t v, int m)
 /*
  * atomic_fetch_or
  */
-ATOMIC_FETCH_OP(or, "orr %[tmp], %[ret], %[v]\n", 1, uint8_t, "b")
-ATOMIC_FETCH_OP(or, "orr %[tmp], %[ret], %[v]\n", 2, uint16_t, "h")
-ATOMIC_FETCH_OP(or, "orr %[tmp], %[ret], %[v]\n", 4, uint32_t, "")
-
 inline uint64_t
 __atomic_fetch_or_8(volatile uint64_t *p, uint64_t v, int m)
 {
@@ -436,10 +271,6 @@ __atomic_fetch_or_8(volatile uint64_t *p, uint64_t v, int m)
 /*
  * atomic_fetch_nand
  */
-ATOMIC_FETCH_OP(nand, "and %[tmp], %[ret], %[v]\nmvn %[tmp], %[tmp]\n", 1, uint8_t, "b")
-ATOMIC_FETCH_OP(nand, "and %[tmp], %[ret], %[v]\nmvn %[tmp], %[tmp]\n", 2, uint16_t, "h")
-ATOMIC_FETCH_OP(nand, "and %[tmp], %[ret], %[v]\nmvn %[tmp], %[tmp]\n", 4, uint32_t, "")
-
 inline uint64_t
 __atomic_fetch_nand_8(volatile uint64_t *p, uint64_t v, int m)
 {
@@ -450,22 +281,3 @@ __atomic_fetch_nand_8(volatile uint64_t *p, uint64_t v, int m)
 	irq_restore(s);
 	return ret;
 }
-
-/*
- * atomic_test_and_set
- */
-inline uint8_t
-__atomic_test_and_set(uint8_t *p, int m)
-{
-	return __atomic_exchange_1(p, 1, m);
-}
-
-/*
- * atomic_clear
- */
-inline void
-__atomic_clear(uint8_t *p, int m)
-{
-	return __atomic_store_1(p, 0, 0);
-}
-
