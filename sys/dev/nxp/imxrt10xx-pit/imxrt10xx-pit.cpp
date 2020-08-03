apexrtos/apex

commit bfc66082fba5c92364fc88b09d52dc7c5f5385af

apex: remove CPPREG
Only one line of code in the usdhc driver was using the constructor
provided by CPPREG which is easily replaced by assigning to the 'r'
member instead.

Remove as this is unnecessary noise at best.

sys/dev/nxp/imxrt10xx-pit/imxrt10xx-pit.cpp
@@ -18,16 +18,11 @@ namespace imxrt10xx {
 
 using namespace std::chrono_literals;
 
-#define CPPREG(name) \
-	name() : r{0} { } \
-	name(auto v) : r{v} { }
-
 /*
  * Registers
  */
 struct pit::regs {
 	union mcr {
-		CPPREG(mcr);
 		struct {
 			uint32_t FRZ : 1;
 			uint32_t MDIS : 1;
@@ -49,7 +44,6 @@ struct pit::regs {
 		uint32_t CVAL;
 
 		union tctrl {
-			CPPREG(tctrl);
 			struct {
 				uint32_t TEN : 1;
 				uint32_t TIE : 1;
@@ -60,8 +54,6 @@ struct pit::regs {
 		} TCTRL;
 
 		union tflg {
-			CPPREG(tflg);
-			struct {
 				uint32_t TIF : 1;
 				uint32_t : 31;
 			};

sys/dev/arm/mps2-uart/mps2-uart.cpp
@@ -10,13 +10,9 @@
 /*
  * Hardware registers
  */
-#define CPPREG(name) \
-	name() : r{0} { } \
-	name(auto v) : r{v} { }
 struct mps2_uart {
 	uint32_t DATA;
 	union state {
-		CPPREG(state);
 		struct {
 			uint32_t TX_FULL : 1;
 			uint32_t RX_FULL : 1;
@@ -27,7 +23,6 @@ struct mps2_uart {
 		uint32_t r;
 	} STATE;
 	union ctrl {
-		CPPREG(ctrl);
 		struct {
 			uint32_t TX_ENABLE : 1;
 			uint32_t RX_ENABLE : 1;
@@ -40,7 +35,6 @@ struct mps2_uart {
 		uint32_t r;
 	} CTRL;
 	union int_status_clear {
-		CPPREG(int_status_clear);
 		struct {
 			uint32_t TX : 1;
 			uint32_t RX : 1;

sys/dev/fsl/lpuart/regs.h
@@ -5,21 +5,12 @@
 #include <endian.h>
 #include <stdint.h>
 
-#if defined(__cplusplus)
-#define CPPREG(name) \
-	name() : r{0} { } \
-	name(auto v) : r{v} { }
-#else
-#define CPPREG(name)
-#endif
-
 /*
  * Hardware register description for Freescale LPUART
  */
 struct lpuart_regs {
 	uint32_t VERID;
 	union lpuart_param {
-		CPPREG(lpuart_param);
 		struct {
 			uint32_t TXFIFO : 8;
 			uint32_t RXFIFO : 8;
@@ -28,7 +19,6 @@ struct lpuart_regs {
 		uint32_t r;
 	} PARAM;
 	union lpuart_global {
-		CPPREG(lpuart_global);
 		struct {
 			uint32_t : 1;
 			uint32_t RST : 1;
@@ -38,7 +28,6 @@ struct lpuart_regs {
 	} GLOBAL;
 	uint32_t PINCFG;
 	union lpuart_baud {
-		CPPREG(lpuart_baud);
 		struct {
 			uint32_t SBR : 13;
 			uint32_t SBNS : 1;
@@ -59,7 +48,6 @@ struct lpuart_regs {
 		uint32_t r;
 	} BAUD;
 	union lpuart_stat {
-		CPPREG(lpuart_stat);
 		struct {
 			uint32_t : 14;
 			uint32_t MA2F : 1;
@@ -84,7 +72,6 @@ struct lpuart_regs {
 		uint32_t r;
 	} STAT;
 	union lpuart_ctrl {
-		CPPREG(lpuart_ctrl);
 		struct {
 			uint32_t PT : 1;
 			uint32_t PE : 1;
@@ -122,7 +109,6 @@ struct lpuart_regs {
 	uint32_t MATCH;
 	uint32_t MODIR;
 	union lpuart_fifo {
-		CPPREG(lpuart_fifo);
 		struct {
 			uint32_t RXFIFOSIZE : 3;
 			uint32_t RXFE : 1;
@@ -144,7 +130,6 @@ struct lpuart_regs {
 		uint32_t r;
 	} FIFO;
 	union lpuart_water {
-		CPPREG(lpuart_water);
 		struct {
 			uint32_t TXWATER : 8;
 			uint32_t TXCOUNT : 8;
@@ -156,7 +141,5 @@ struct lpuart_regs {
 };
 static_assert(sizeof(struct lpuart_regs) == 0x30, "");
 static_assert(BYTE_ORDER == LITTLE_ENDIAN, "");
-
-#undef CPPREG
 
 #endif

sys/dev/fsl/usb2-udc/usb2-udc.cpp
@@ -25,12 +25,8 @@ class fsl_usb2_transaction;
 /*
  * regs
  */
-#define CPPREG(name) \
-	name() : r{0} { } \
-	name(auto v) : r{v} { }
 struct regs {
 	union id {
-		CPPREG(id);
 		struct {
 			uint32_t ID : 6;
 			uint32_t : 2;
@@ -117,7 +113,6 @@ struct regs {
 	uint16_t DCIVERSION;
 	uint16_t : 16;
 	union dccparams {
-		CPPREG(dccparams);
 		struct {
 			uint32_t DEN : 5;
 			uint32_t : 2;
@@ -134,7 +129,6 @@ struct regs {
 	uint32_t : 32;
 	uint32_t : 32;
 	union usbcmd {
-		CPPREG(usbcmd);
 		/* The IMXRT manual is wrong and the ATDTW bit is in position
 		 * 14 like previous generations of hardware. */
 		struct {
@@ -158,7 +152,6 @@ struct regs {
 		uint32_t r;
 	} USBCMD;
 	union usbsts {
-		CPPREG(usbsts);
 		struct {
 			uint32_t UI : 1;
 			uint32_t UEI : 1;
@@ -179,7 +172,6 @@ struct regs {
 		uint32_t r;
 	} USBSTS;
 	union usbintr {
-		CPPREG(usbintr);
 		struct {
 			uint32_t UE : 1;
 			uint32_t UEE : 1;
@@ -202,7 +194,6 @@ struct regs {
 	uint32_t FRINDEX;
 	uint32_t : 32;
 	union deviceaddr {
-		CPPREG(deviceaddr);
 		struct {
 			uint32_t : 24;
 			uint32_t USBADRA : 1;
@@ -222,7 +213,6 @@ struct regs {
 	uint32_t ENDPTNAKEN;
 	uint32_t : 32;
 	union portsc1 {
-		CPPREG(portsc1);
 		struct {
 			uint32_t CCS : 1;
 			uint32_t : 3;
@@ -258,7 +248,6 @@ struct regs {
 	uint32_t : 32;
 	uint32_t OTGSC;
 	union usbmode {
-		CPPREG(usbmode);
 		struct {
 			uint32_t CM : 2;
 			uint32_t ES : 1;
@@ -274,7 +263,6 @@ struct regs {
 	uint32_t ENDPTSTAT;
 	uint32_t ENDPTCOMPLETE;
 	union endptctrl {
-		CPPREG(endptctrl);
 		struct {
 			uint32_t RXS : 1;
 			uint32_t RXD : 1;   /* not for endpoint 0 */

sys/dev/fsl/usdhc/usdhc.cpp
@@ -21,12 +21,8 @@ namespace {
 /*
  * regs
  */
-#define CPPREG(name) \
-	name() : r{0} { } \
-	name(auto v) : r{v} { }
 struct regs {
 	union int_flags {
-		CPPREG(int_flags)
 		struct {
 			uint32_t CC : 1;
 			uint32_t TC : 1;
@@ -61,7 +57,6 @@ struct regs {
 	};
 	uint32_t DS_ADDR;
 	union blk_att {
-		CPPREG(blk_att)
 		struct {
 			uint32_t BLKSIZE : 13;
 			uint32_t : 3;
@@ -71,7 +66,6 @@ struct regs {
 	} BLK_ATT;
 	uint32_t CMD_ARG;
 	union cmd_xfr_typ {
-		CPPREG(cmd_xfr_typ)
 		struct {
 			uint32_t : 16;
 			uint32_t RSPTYP : 2;
@@ -91,7 +85,6 @@ struct regs {
 	uint32_t CMD_RSP3;
 	uint32_t DATA_BUFF_ACC_PORT;
 	union pres_state {
-		CPPREG(pres_state);
 		struct {
 			uint32_t CIHB : 1;
 			uint32_t CDIHB : 1;
@@ -119,7 +112,6 @@ struct regs {
 		uint32_t r;
 	} PRES_STATE;
 	union prot_ctrl {
-		CPPREG(prot_ctrl);
 		struct {
 			uint32_t LCTL : 1;
 			uint32_t DTW : 2;
@@ -145,7 +137,6 @@ struct regs {
 		uint32_t r;
 	} PROT_CTRL;
 	union sys_ctrl {
-		CPPREG(sys_ctrl);
 		struct {
 			uint32_t : 4;
 			uint32_t DVS : 4;
@@ -166,7 +157,6 @@ struct regs {
 	int_flags INT_STATUS_EN;
 	int_flags INT_SIGNAL_EN;
 	union autocmd12_err_status {
-		CPPREG(autocmd12_err_status);
 		struct {
 			uint32_t AC12NE : 1;
 			uint32_t AC12TOE : 1;
@@ -183,7 +173,6 @@ struct regs {
 		uint32_t r;
 	} AUTOCMD12_ERR_STATUS;
 	union host_ctrl_cap {
-		CPPREG(host_ctrl_cap);
 		struct {
 			uint32_t SDR50_SUPPORT : 1;
 			uint32_t SDR104_SUPPORT : 1;
@@ -207,7 +196,6 @@ struct regs {
 		uint32_t r;
 	} HOST_CTRL_CAP;
 	union wtmk_lvl {
-		CPPREG(wtmk_lvl);
 		struct {
 			uint32_t RD_WML : 8;
 			uint32_t RD_BRST_LEN : 5;
@@ -219,7 +207,6 @@ struct regs {
 		uint32_t r;
 	} WTMK_LVL;
 	union mix_ctrl {
-		CPPREG(mix_ctrl);
 		struct {
 			uint32_t DMAEN : 1;
 			uint32_t BCEN : 1;
@@ -246,7 +233,6 @@ struct regs {
 	uint32_t DLL_CTRL;
 	uint32_t DLL_STATUS;
 	union clk_tune_ctrl_status {
-		CPPREG(clk_tune_ctrl_status);
 		struct {
 			uint32_t DLY_CELL_SET_POS : 4;
 			uint32_t DLY_CELL_SET_OUT : 4;
@@ -281,7 +267,6 @@ struct regs {
 	uint32_t : 32;
 	uint32_t : 32;
 	union vend_spec {
-		CPPREG(vend_spec);
 		struct {
 			uint32_t : 1;
 			uint32_t VSELECT : 1;
@@ -298,7 +283,6 @@ struct regs {
 	} VEND_SPEC;
 	uint32_t MMC_BOOT;
 	union vend_spec2 {
-		CPPREG(vend_spec2);
 		struct {
 			uint32_t : 3;
 			uint32_t CARD_INT_D3_TEST : 1;
@@ -316,7 +300,6 @@ struct regs {
 		uint32_t r;
 	} VEND_SPEC2;
 	union tuning_ctrl {
-		CPPREG(tuning_ctrl);
 		struct {
 			uint32_t TUNING_START_TAP : 8;
 			uint32_t TUNING_COUNTER : 8;
@@ -546,7 +529,7 @@ fsl_usdhc::v_reset()
 	}());
 
 	/* Initialise interrupt mask. */
-	int_mask = 0u;
+	int_mask.r = 0;
 	int_mask.CC = 1;
 	int_mask.TC = 1;
 	int_mask.CINS = 1;

sys/dev/nxp/imxrt10xx-ocotp/imxrt10xx-ocotp.cpp
@@ -70,16 +70,11 @@ clocks(unsigned long clock, std::chrono::nanoseconds t)
 
 namespace imxrt10xx {
 
-#define CPPREG(name) \
-	name() : r{0} { } \
-	name(auto v) : r{v} { }
-
 /*
  * Registers
  */
 struct ocotp::regs {
 	union ctrl {
-		CPPREG(ctrl);
 		struct {
 			uint32_t ADDR : 6;
 			uint32_t : 2;
@@ -96,7 +91,6 @@ struct ocotp::regs {
 	ctrl CTRL_CLR;
 	ctrl CTRL_TOG;
 	union timing {
-		CPPREG(timing);
 		struct {
 			uint32_t STROBE_PROG : 12;
 			uint32_t RELAX : 4;
@@ -114,7 +108,6 @@ struct ocotp::regs {
 	uint32_t : 32;
 	uint32_t : 32;
 	union read_ctrl {
-		CPPREG(read_ctrl);
 		struct {
 			uint32_t READ_FUSE : 1;
 			uint32_t : 31;
@@ -133,7 +126,6 @@ struct ocotp::regs {
 	uint32_t : 32;
 	uint32_t : 32;
 	union scs {
-		CPPREG(scs);
 		struct {
 			uint32_t HAB_JDE : 1;
 			uint32_t : 30;
@@ -153,7 +145,6 @@ struct ocotp::regs {
 	uint32_t : 32;
 	uint32_t : 32;
 	union version {
-		CPPREG(version);
 		struct {
 			uint32_t STEP : 16;
 			uint32_t MINOR : 8;
@@ -163,7 +154,6 @@ struct ocotp::regs {
 	} VERSION;
 	uint32_t reserved[27];
 	union timing2 {
-		CPPREG(timing2);
 		struct {
 			uint32_t RELAX_PROG : 12;
 			uint32_t : 4;

commit 07c7f5cdf036c082ade005313c12f029b3f19127

imxrt/pit: fix build failure
Introduced by bfc66082fba5c92364fc88b09d52dc7c5f5385af. Oops.

sys/dev/nxp/imxrt10xx-pit/imxrt10xx-pit.cpp
@@ -54,6 +54,7 @@ struct pit::regs {
 		} TCTRL;
 
 		union tflg {
+			struct {
 				uint32_t TIF : 1;
 				uint32_t : 31;
 			};