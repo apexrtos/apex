apexrtos/apex

commit 65cef4d62a6974538d10e11cd1b2f5bf743b33d0

tty: tstchar no longer exists
tstchar was a Prex C library function which no longer exists.

sys/dev/tty/tty.cpp
@@ -242,10 +242,6 @@ tty::read(file *f, std::span<std::byte> buf)
 	std::unique_lock sl{state_lock_};
 	std::unique_lock rl{rxq_lock_};
 
-	/* tstchar calls with a NULL buffer */
-	if (f->f_flags & O_NONBLOCK && !rx_avail())
-		return -EAGAIN;
-
 	if (!data(buf))
 		return DERR(-EFAULT);
 

commit 28700368e824fb16a76f629fc81d5cd601dfac1a

tty: fix O_NONBLOCK read
Broken in 65cef4d62a6974538d10e11cd1b2f5bf743b33d0.

sys/dev/tty/tty.cpp
@@ -252,6 +252,9 @@ tty::read(file *f, std::span<std::byte> buf)
 
 	std::unique_lock sl{state_lock_};
 
+	if (f->f_flags & O_NONBLOCK && !rx_avail())
+		return -EAGAIN;
+
 	/* wait for input */
 	while (!rx_avail()) {
 		/*