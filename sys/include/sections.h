042362c5bd3bced2449398ffef4eca7d6dc6596b
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2021-01-28 13:02:04 +1100

    apex: use NOLOAD section attribute in linker scripts
    Section attribute hacks don't work with clang. This is a cleaner
    solution anyway as it no longer requires injection of a comment into the
    section attribute.
    
    lld also requires the NOLOAD attribute on our stack sections to prevent
    it allocating file space to them.

--- sys/include/sections.h
+++ sys/include/sections.h
@@ -4,12 +4,7 @@
 #define __fast_data __attribute__((section(".fast_data")))
 #define __fast_rodata __attribute__((section(".fast_rodata")))
 #define __fast_text __attribute__((section(".fast_text")))
-
-#ifdef __arm__
-#define __fast_bss	__attribute__((section(".fast_bss, \"aw\", %nobits //")))
-#else
-#define __fast_bss	__attribute__((section(".fast_bss, \"aw\", @nobits //")))
-#endif
+#define __fast_bss	__attribute__((section(".fast_bss")))
 
 #endif /* !sections_h */
 
