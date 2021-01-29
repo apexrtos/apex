6b6e13c82bd8965802bc9bcfc5098d33c093dee7
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2021-01-29 15:44:28 +1100

    arm/v7m/atomic: clang support

--- sys/arch/arm/v7m/atomic.c
+++ sys/arch/arm/v7m/atomic.c
@@ -15,14 +15,14 @@
 /*
  * atomic_load
  */
-inline uint64_t
+uint64_t
 __atomic_load_8(const volatile void *p, int m)
 {
 	/* ldrd is restarted on interrupt */
 	const volatile uint64_t *p64 = p;
 	uint64_t tmp;
 	asm volatile(
-		"ldrd %[r], %H[r], %[p]\n"
+		"ldrd %Q[r], %R[r], %[p]\n"
 		: [r]"=lh"(tmp)
 		: [p]"m"(*p64)
 		: "memory"
@@ -30,8 +30,9 @@ __atomic_load_8(const volatile void *p, int m)
 	return tmp;
 }
 
-inline void
-__atomic_load(size_t len, const void *p, void *r, int m)
+#pragma redefine_extname atomic_load __atomic_load
+void
+atomic_load(size_t len, const void *p, void *r, int m)
 {
 	const int s = irq_disable();
 	memcpy(r, p, len);
@@ -41,7 +42,7 @@ __atomic_load(size_t len, const void *p, void *r, int m)
 /*
  * atomic_store
  */
-inline void
+void
 __atomic_store_8(volatile void *p, uint64_t v, int m)
 {
 	volatile uint64_t *p64 = p;
@@ -49,7 +50,7 @@ __atomic_store_8(volatile void *p, uint64_t v, int m)
 	asm volatile(
 		"mrs %[s], PRIMASK\n"
 		"cpsid i\n"
-		"strd %[v], %H[v], %[p]\n"
+		"strd %Q[v], %R[v], %[p]\n"
 		"msr PRIMASK, %[s]\n"
 		: [s]"=&lh"(s), [p]"=m"(*p64)
 		: [v]"lh"(v)
@@ -57,8 +58,9 @@ __atomic_store_8(volatile void *p, uint64_t v, int m)
 	);
 }
 
-inline void
-__atomic_store(size_t len, void *p, const void *v, int m)
+#pragma redefine_extname atomic_store __atomic_store
+void
+atomic_store(size_t len, void *p, const void *v, int m)
 {
 	const int s = irq_disable();
 	memcpy(p, v, len);
@@ -68,7 +70,7 @@ __atomic_store(size_t len, void *p, const void *v, int m)
 /*
  * atomic_exchange
  */
-inline uint64_t
+uint64_t
 __atomic_exchange_8(volatile void *p, uint64_t v, int m)
 {
 	volatile uint64_t *p64 = p;
@@ -77,8 +79,8 @@ __atomic_exchange_8(volatile void *p, uint64_t v, int m)
 	asm volatile(
 		"mrs %[s], PRIMASK\n"
 		"cpsid i\n"
-		"ldrd %[ret], %H[ret], %[p]\n"
-		"strd %[v], %H[v], %[p]\n"
+		"ldrd %Q[ret], %R[ret], %[p]\n"
+		"strd %Q[v], %R[v], %[p]\n"
 		"msr PRIMASK, %[s]\n"
 		: [s]"=&lh"(s), [p]"=m"(*p64), [ret]"=&lh"(ret)
 		: [v]"lh"(v), "m"(*p64)
@@ -87,8 +89,9 @@ __atomic_exchange_8(volatile void *p, uint64_t v, int m)
 	return ret;
 }
 
-inline void
-__atomic_exchange(size_t len, void *p, const void *v, void *r, int m)
+#pragma redefine_extname atomic_exchange __atomic_exchange
+void
+atomic_exchange(size_t len, void *p, const void *v, void *r, int m)
 {
 	const int s = irq_disable();
 	memcpy(r, p, len);
@@ -99,7 +102,7 @@ __atomic_exchange(size_t len, void *p, const void *v, void *r, int m)
 /*
  * atomic_compare_exchange
  */
-inline bool
+bool
 __atomic_compare_exchange_8(volatile void *p, const void *e, uint64_t d,
     bool weak, int sm, int fm)
 {
@@ -113,8 +116,9 @@ __atomic_compare_exchange_8(volatile void *p, const void *e, uint64_t d,
 	return ret;
 }
 
-inline bool
-__atomic_compare_exchange(size_t len, void *p, const void *e, void *d,
+#pragma redefine_extname atomic_compare_exchange __atomic_compare_exchange
+bool
+atomic_compare_exchange(size_t len, void *p, const void *e, void *d,
     int sm, int fm)
 {
 	bool ret;
@@ -128,7 +132,7 @@ __atomic_compare_exchange(size_t len, void *p, const void *e, void *d,
 /*
  * atomic_add_fetch
  */
-inline uint64_t
+uint64_t
 __atomic_add_fetch_8(volatile void *p, uint64_t v, int m)
 {
 	volatile uint64_t *p64 = p;
@@ -142,7 +146,7 @@ __atomic_add_fetch_8(volatile void *p, uint64_t v, int m)
 /*
  * atomic_sub_fetch
  */
-inline uint64_t
+uint64_t
 __atomic_sub_fetch_8(volatile void *p, uint64_t v, int m)
 {
 	volatile uint64_t *p64 = p;
@@ -156,7 +160,7 @@ __atomic_sub_fetch_8(volatile void *p, uint64_t v, int m)
 /*
  * atomic_and_fetch
  */
-inline uint64_t
+uint64_t
 __atomic_and_fetch_8(volatile void *p, uint64_t v, int m)
 {
 	volatile uint64_t *p64 = p;
@@ -170,7 +174,7 @@ __atomic_and_fetch_8(volatile void *p, uint64_t v, int m)
 /*
  * atomic_xor_fetch
  */
-inline uint64_t
+uint64_t
 __atomic_xor_fetch_8(volatile void *p, uint64_t v, int m)
 {
 	volatile uint64_t *p64 = p;
@@ -184,7 +188,7 @@ __atomic_xor_fetch_8(volatile void *p, uint64_t v, int m)
 /*
  * atomic_or_fetch
  */
-inline uint64_t
+uint64_t
 __atomic_or_fetch_8(volatile void *p, uint64_t v, int m)
 {
 	volatile uint64_t *p64 = p;
@@ -198,7 +202,7 @@ __atomic_or_fetch_8(volatile void *p, uint64_t v, int m)
 /*
  * atomic_nand_fetch
  */
-inline uint64_t
+uint64_t
 __atomic_nand_fetch_8(volatile void *p, uint64_t v, int m)
 {
 	volatile uint64_t *p64 = p;
@@ -212,7 +216,7 @@ __atomic_nand_fetch_8(volatile void *p, uint64_t v, int m)
 /*
  * atomic_fetch_add
  */
-inline uint64_t
+uint64_t
 __atomic_fetch_add_8(volatile void *p, uint64_t v, int m)
 {
 	volatile uint64_t *p64 = p;
@@ -227,7 +231,7 @@ __atomic_fetch_add_8(volatile void *p, uint64_t v, int m)
 /*
  * atomic_fetch_sub
  */
-inline uint64_t
+uint64_t
 __atomic_fetch_sub_8(volatile void *p, uint64_t v, int m)
 {
 	volatile uint64_t *p64 = p;
@@ -242,7 +246,7 @@ __atomic_fetch_sub_8(volatile void *p, uint64_t v, int m)
 /*
  * atomic_fetch_and
  */
-inline uint64_t
+uint64_t
 __atomic_fetch_and_8(volatile void *p, uint64_t v, int m)
 {
 	volatile uint64_t *p64 = p;
@@ -257,7 +261,7 @@ __atomic_fetch_and_8(volatile void *p, uint64_t v, int m)
 /*
  * atomic_fetch_xor
  */
-inline uint64_t
+uint64_t
 __atomic_fetch_xor_8(volatile void *p, uint64_t v, int m)
 {
 	volatile uint64_t *p64 = p;
@@ -272,7 +276,7 @@ __atomic_fetch_xor_8(volatile void *p, uint64_t v, int m)
 /*
  * atomic_fetch_or
  */
-inline uint64_t
+uint64_t
 __atomic_fetch_or_8(volatile void *p, uint64_t v, int m)
 {
 	volatile uint64_t *p64 = p;
@@ -287,7 +291,7 @@ __atomic_fetch_or_8(volatile void *p, uint64_t v, int m)
 /*
  * atomic_fetch_nand
  */
-inline uint64_t
+uint64_t
 __atomic_fetch_nand_8(volatile void *p, uint64_t v, int m)
 {
 	volatile uint64_t *p64 = p;
