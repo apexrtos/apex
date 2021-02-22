6c6f9115b5568f18d7ff74d07adcedf2ba612056
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2021-02-22 16:27:37 +1100

    boot: makefile support for c++

--- boot/boot.mk
+++ boot/boot.mk
@@ -13,16 +13,21 @@ endif
 TYPE := binary
 TARGET := boot
 LDSCRIPT := arch/$(CONFIG_ARCH)/boot.ld
-CFLAGS += -Wframe-larger-than=384
-CFLAGS += -fno-pie
-CFLAGS += $(CONFIG_APEX_CFLAGS)
-CFLAGS_$(COMPILER) += $(CONFIG_APEX_CFLAGS_$(COMPILER))
+FLAGS += -Wframe-larger-than=384
+FLAGS += -fno-pie
+FLAGS += $(CONFIG_APEX_CFLAGS) $(CONFIG_APEX_CFLAGS_$(COMPILER))
+CFLAGS += $(FLAGS)
+CXXFLAGS += $(FLAGS) -nostdinc++ -fno-exceptions -std=gnu++20
+CXXFLAGS_gcc += -fno-use-cxa-atexit
+CXXFLAGS_clang += -fno-c++-static-destructors
 LDFLAGS += -z max-page-size=32 -nostartfiles -nostdlib -static -no-pie
+DEFS += -D_GNU_SOURCE
 LIBS := ../libc/libc.a \
     $(shell $(CROSS_COMPILE)$(COMPILER) --print-libgcc-file-name)
 
 INCLUDE := \
     include \
+    $(CONFIG_APEXDIR)/libc++/include \
     $(CONFIG_APEXDIR) \
     $(CONFIG_BUILDDIR) \
 
